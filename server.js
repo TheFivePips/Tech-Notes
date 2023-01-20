54734796be386a872873627644ec190612feb7a2
Sam Gardner <samgardner20@gmail.com>
2023-01-20 11:06:14 -0500

    added in some middleware to log and handler errors and protect our api from outside sources

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express')
-
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3500
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const corsOptions = require('./config/corsOptions')
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use(logger)
+
+app.use(cors(corsOptions))
+
+app.use(express.json())
+
+app.use(cookieParser())
+
+
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
 
@@ -21,5 +34,7 @@ app.all('*', (req, res) => {
         res.type('txt').send('404 Not Found')
     }
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`server running on ${PORT}`))
