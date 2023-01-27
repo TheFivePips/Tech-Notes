1863c9d3b09512c7bc3835608228fb37bf74999a
Sam Gardner <samgardner20@gmail.com>
2023-01-27 12:42:34 -0500

    adjusting cors options

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,7 @@
 
-const allowedOrigins = ["https://technotes-5j75.onrender.com"];
+const allowedOrigins = [
+  "https://technotes-5j75.onrender.com",
+  "https://technotes.onrender.com",
+];
 
 module.exports = allowedOrigins
